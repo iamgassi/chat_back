9b177d3d40d2bdce4e579caf441c40603ba7addf
iamgassi <gauravgassi123@gmail.com>
2022-10-21 11:54:44 +0530

    $

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const app =express();
 const userModel =require('./database/models/user')
 const chatIdModel=require('./database/models/chatId')
 const messageModel=require('./database/models/messages')
+const cookieParser = require('cookie-parser')
 const corsModule=require('cors')
-const db=require('./database/index')
+const db=require('./database/index');
+const { json } = require('express');
 const server=require('http').createServer(app)
 const io = require("socket.io")(server, {
 	cors: {
@@ -16,18 +18,28 @@ db.init()  //for DB connection
 
 app.use(corsModule())
 app.use(express.json())
+app.use(express.text())
+app.use(cookieParser());
 
-io.on('connection', (socket) => {
-	console.log(socket.connected)
-socket.on('chat message', function(msg) {
+//------Socket.io-----------------------------------------------
+io.on('connection',async (socket) => {
+	console.log(socket.id)
+	const userId=socket.id
+	socket.join(userId)
+	console.log("Rooms in socket",socket.rooms)
+
+    socket.on('chat message', function(msg) {
 	console.log("msg recived")
-	io.emit('message',msg)
+	// io.emit('message',msg)
+	io.to(userId).emit('message',msg);
   });
 
   socket.on('disconnect',()=>{
 	console.log("disconnected")
   })
 })
+
+//------------------------------------------------------
 
 // getting user
 app.route('/user').get((req,res)=>{
@@ -120,46 +132,127 @@ app.route('/userToUser').get((req,res)=>{
 
 }).post((req,res)=>{
 	const response=req.body
+	console.log(response)
+	const user1=response.user1;
+	const user2=response.user2;
+	
+    chatIdModel.find({})
+	.then(function(data){
+	// IF no user present then create
+		if(!data.length)
+		{
+			console.log("No User Present-------------------------------")
+			chatIdModel.create(
+				{
+						user1:user1,
+						user2:user2
+
+				}
+			)
+			.then(()=>
+			{  
+				 res.json({ msg:"Successfully Created"});
+				 console.log("successfully created")
+			})
+			.catch((err)=>
+			{
+				console.log(err)
+				res.json({ msg:"ChatId Already Exist!!"})
+				console.log("ChatId already exist")
+			})
+		}
+		//check for existing User.
+		else 
+		{  console.log("Atleast Onechatid present----------------------------------")
+		   chatIdModel.find({$or:[
+			{$and:[{user1:user1},{user2:user2}]},
+			{$and:[{user2:user1},{user1:user2}]}
+		  ]})
+		  .then((data)=>{
+			if(!data.length)
+			{
+				console.log("No exists then create ")
+				chatIdModel.create(
+					{
+							user1:user1,
+							user2:user2
+					}
+				)
+				.then(()=>
+				{  
+					 res.json({ msg:"Successfully Created"});
+					 console.log("successfully created")
+					
+				})
+			}
+			else
+			{
+				console.log("No need to create Return")
+				return
+			}
+			// return;
+			// chatIdModel.updateOne({_id:data.id},{$set:{messages:status}})	
+		})
+		.catch(err=>{
+			console.log(err)
+		})
+
+		}
+     
+	})
+	
+})
+app.post('/getMessages',(req,res)=>{
+    const response=req.body
+	console.log(response)
 	const user1=response.user1;
 	const user2=response.user2;
 
-  messageModel.find({})
-  .then(function(data){
-	res.json(data)
-	if(data === null)
-	{
-	  res.end("No data")
-	} 
-  })
-  .catch(function(err){
-	res.json({msg:err});	
-        console.log(err)
-  })
-  if(user)
-  {
-					chatIdModel.create(
-						{
-							user1:user1,
-							user2:user2
+	chatIdModel.find({$or:[
+		{$and:[{user1:user1},{user2:user2}]},
+		{$and:[{user2:user1},{user1:user2}]}
+	  ]})
+	  .then((data)=>{
+		if(!data[0]) return
+		//   console.log("MEssages",data[0].messages)
+		res.send(data[0].messages)
+	  })
 
-						}
-					)
-					.then(()=>
-					{  
-						 res.json({ msg:"Successfully registered"});
-					})
-					.catch((err)=>
-					{
-						console.log(err)
-						res.json({ msg:"User Already Exist!!"})
-					})
-		// })
-	}
-	else
-	{
-    res.json({ msg:"Enter a valid detail || Password mismatch"})
-	}
 })
+
+app.post('/message',(req,res)=>{
+		const response=req.body
+		const user1=response.user2user.user1;
+	    const user2=response.user2user.user2;
+		const msg=response.msg
+		console.log("Recived Msg is:",msg)
+
+		chatIdModel.findOne({$or:[
+			{$and:[{user1:user1},{user2:user2}]},
+			{$and:[{user2:user1},{user1:user2}]}
+		  ]})
+		  .then((data)=>{
+			//  chatIdModel.create({
+			// 	messages:msg
+			//  })
+			console.log(data,"TO be change")
+			console.log("ID",data.id)
+			chatIdModel.findByIdAndUpdate(
+				{ _id:data._id },
+				{ $push: { messages: msg } }
+				)
+				.then(data=>{
+				 console.log(msg)
+				console.log("After Update",data)
+			 })
+			console.log(data)
+		  })
+		  
+
+	
+	
+}
+)
 
 
 
--- database/models/chatId.js
+++ database/models/chatId.js
@@ -1,31 +1,13 @@
 const mongoose = require("mongoose");
 
 const chatIdSchema  = new mongoose.Schema({
-    chatid:{
-        type:Array,
-        unique:true,
-        required:true,
-                user1:{
-                    type:String,
-                    unique:true,
-                    required:true,
-                    
-                },
-                user2:{
-                    type:String,
-                    unique:true,
-                    required:true,
-                }
-    },
- created_at:
-{ 
-    type: Date,
-    required: true,
-    default: Date.now
- },
+               user1:{type:String},
+               user2:{type:String},
+               messages:{type:Array}
 },
  )
 
  const chatIdModel = mongoose.model('ChatId', chatIdSchema);
 
  module.exports=chatIdModel
+ 
